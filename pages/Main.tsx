004d1eef4b83464383df9978e17a345e3292c6e5
Kurkuza <danilsherrbakov1993@gmail.com>
2022-08-04 13:24:52 +0300

    feat pagination

--- pages/Main.tsx
+++ pages/Main.tsx
@@ -1,23 +1,55 @@
-import React from 'react'
-import { getStaticProps } from './_app'
+import Masonry from '@mui/lab/Masonry'
+import { Container, Pagination, Typography } from '@mui/material'
+import React, { FC, useEffect, useState } from 'react'
+import { Character, GetCharacterResults } from '../types'
 
-function Main({ characters }: any) {
-  console.log('> characters', characters)
-	console.log('> getStaticProps', getStaticProps)
+function Main() {
+	const [characters, setCharacters] = useState<Character[]>([])
+	const [page, setPage] = React.useState(1)
+
+	useEffect(() => {
+		const fetchCharacters = async () => {
+			const res = await fetch(
+				`https://rickandmortyapi.com/api/character/?page=${page}`,
+			)
+			const { results }: GetCharacterResults = await res.json()
+
+			setCharacters(results)
+		}
+		fetchCharacters()
+	}, [page])
+
+	const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+		setPage(value)
+	}
 
 	return (
-		<div>
-			hiiiiiiiiiiiiiiiiiiiiiiiii
-			
-			{/* {characters.map((character) => {
-				return (
-					<div key={character.id}>
-						{character.name}
-						<img src={character.image} />
+		<Container maxWidth='xl'>
+			<Typography>Page: {page}</Typography>
+			<Pagination
+				count={10}
+				variant='outlined'
+				color='primary'
+				onChange={handleChange}
+			/>
+
+			<Masonry columns={5} spacing={2}>
+				{characters.map((character, index) => (
+					<div key={index}>
+						<Typography>{character.name}</Typography>
+						<img
+							src={character.image}
+							srcSet={`${character.image}?w=162&auto=format&dpr=2 2x`}
+							style={{
+								borderBottomLeftRadius: 4,
+								borderBottomRightRadius: 4,
+								width: '277px',
+							}}
+						/>
 					</div>
-				)
-			})} */}
-		</div>
+				))}
+			</Masonry>
+		</Container>
 	)
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,20 @@
+import { ThemeProvider } from '@mui/material'
+import { unstable_createMuiStrictModeTheme } from '@mui/material/styles'
 import { GetStaticProps } from 'next'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 import { GetCharacterResults } from '../types'
 import Main from './Main'
 
+const theme = unstable_createMuiStrictModeTheme()
+
 function MyApp() {
 	return (
 		<>
-		<Main />
+			<ThemeProvider theme={theme}>
+				<Main />
+			</ThemeProvider>
 		</>
 	)
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-	const res = await fetch('https://rickandmortyapi.com/api/character')
-	const { results }: GetCharacterResults = await res.json()
-
-	const characters = {
-		props: { characters: results },
-	}
-
-	return characters
 }
 export default MyApp
